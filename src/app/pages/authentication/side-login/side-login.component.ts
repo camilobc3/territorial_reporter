5d465cbe6058e6d020c2cdd5b4caa12f7d3cc5c3
devhytz <devhytz@gmail.com>
2026-06-16 10:53:58 -0500

    Fix: diseño de login

--- src/app/pages/authentication/side-login/side-login.component.ts
+++ src/app/pages/authentication/side-login/side-login.component.ts
@@ -14,6 +14,7 @@ import { SecurityService } from 'src/app/services/security.service';
     MaterialModule
   ],
   templateUrl: './side-login.component.html',
+  styleUrls: ['./side-login.component.scss']
 })
 export class AppSideLoginComponent {
 
